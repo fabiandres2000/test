69cd475b1c0bbedc7788f9b1b51bcfeaebd6a264
Fabian Quintero <grovveip@gmail.com>
2022-03-25 15:25:24 -0500

    df

--- php/guardar_respuestas.php
+++ php/guardar_respuestas.php
@@ -1,21 +1,22 @@
 <?php 
     $paso =  $_GET['paso'];
+    
     if($paso == "paso_1"){
-        echo "paso 1";
+       
     }
     if($paso == "paso_2"){
-        echo "paso 2";
+       
     }
     if($paso == "paso_3"){
-        echo "paso 3";
+       
     }
     if($paso == "paso_4"){
-        echo "paso 4";
+        
     }
     if($paso == "paso_5"){
-        echo "paso 5";
+       
     }
     if($paso == "paso_6"){
-        echo "paso 6";
+        
     }
 ?>
